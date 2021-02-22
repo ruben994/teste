3c1a2171ffd60b2b4d4b6836c67324624499bf27
Rúben Santos <rsantos@negocioselectronicos.biz>
2021-02-22 15:20:10 +0000

    First Version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FraseComponent } from './frase/frase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FraseComponent
   ],
   imports: [
     BrowserModule,
